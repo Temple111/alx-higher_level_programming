dcd6deb3c0442dc63e1370ed5c735e0d89f9e436
Temple111 <eseigbetemple122@gmail.com>
2023-05-08 04:40:56 -0700

    correction of 0x00-python-hello_world

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -6,4 +6,3 @@ string = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(string)
 sys.exit(1)
 
-# this could also be written like this sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n'), no need to use the message as a variable that stores the string
