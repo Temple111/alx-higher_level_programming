d2654f715ebd719ce27e6bbdd5cd25befc79a2b0
Temple111 <eseigbetemple122@gmail.com>
2023-05-08 04:49:18 -0700

    0x00-python-hello_world

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -2,7 +2,6 @@
 
 import sys
 
-string = 'and that piece of art is useful - Dora Korpar, 2015-10-19\n'
-sys.stderr.write(string)
+sys.stderr.write('and that piece of art is useful - Dora Korpar, 2015-10-19\n')
 sys.exit(1)
 
