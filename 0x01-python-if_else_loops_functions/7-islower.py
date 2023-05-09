cbf6554696eacc650a648dd135a7271e61b1a883
Temple111 <eseigbetemple122@gmail.com>
2023-05-09 14:05:55 +0000

    1

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
-def islower(ch):
+def islower(c):
 
-    if ord(ch) >= 97 and ord(ch) <= 122:
+    if ord(c) >= 97 and ord(c) <= 122:
 	return True
     else:
         return False
