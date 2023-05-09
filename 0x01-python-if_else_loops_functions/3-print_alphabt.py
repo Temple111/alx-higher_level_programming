8043898fb5dd1a1141c4fa5259d39c9ecb5e059d
Temple111 <eseigbetemple122@gmail.com>
2023-05-09 14:36:34 +0000

    0x01. Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
 for alpha in range(97, 123):
-    if chr(alpha) is not 'q' and chr(alpha) is not 'e':
-        print("{}".format(chr(alpha)), end="")
+    if (alpha != 101 and alpha != 113):
+        print("{:c}".format(alpha), end='')
 
