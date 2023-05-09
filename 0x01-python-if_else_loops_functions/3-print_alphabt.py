4fb9c304006d8312c34336a08a7811b40881319c
Temple111 <eseigbetemple122@gmail.com>
2023-05-09 13:20:11 +0000

    1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for alphas in range(97, 123):
-	if chr(alphas) is not 'q' and chr(alphas) is not 'e':
-	   print("{:c}".format(alphas), end='')
+    if chr(alphas) is not 'q' and chr(alphas) is not 'e':
+	print("{:c}".format(alphas), end='')
