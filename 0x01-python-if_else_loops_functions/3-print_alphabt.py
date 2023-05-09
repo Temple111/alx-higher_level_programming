4fd89a4b3192d78a122a2d329d1a1848ca5cb026
Temple111 <eseigbetemple122@gmail.com>
2023-05-09 14:16:04 +0000

    1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 for alphas in range(97, 123):
     if chr(alphas) is not 'q' and chr(alphas) is not 'e':
-	print("{:c}".format(alphas), end='')
+        print("{:c}".format(alphas), end='')
--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@
 def uppercase(str):
 
     for char in str:
-        if ord(char) >= 97 and ord(char) <= 122:
-           char = chr(ord(char) - 32)
-        print("{}".format(char), end="")
+       if ord(char) >= 97 and ord(char) <= 122:
+    char = chr(ord(char) - 32)
+    print("{}".format(char), end="")
     print("")
