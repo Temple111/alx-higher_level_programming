eee8b96e6e8a6cb80d8ffdbdd915150e30e8d3c6
Temple111 <eseigbetemple122@gmail.com>
2023-05-11 13:08:33 +0000

    4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
+import hidden_4
+
+
 if __name__ == "__main__":
-    import hidden_4
+   
     # Print sorted name from directory
     for name in sorted(dir(hidden_4)):
         # print only names that do not start with __
