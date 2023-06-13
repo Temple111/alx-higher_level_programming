5deec7b6672be4357e9295ce5a8232cda50fd623
Temple111 <eseigbetemple122@gmail.com>
2023-06-13 06:04:27 -0700

    0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    with open(filename, 'r') as k:
-        for ia in k:
-            print(ia, end="")
-    k.closed
+    with open(filename, 'r') as f:
+        for li in f:
+            print(li, end="")
+    f.closed
